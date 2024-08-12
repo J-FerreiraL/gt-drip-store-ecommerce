0d9a747554ee3b6089cb20369d4939837a33dce0
João Ferreira Linhares <102161649+JoaoFerreira2002@users.noreply.github.com>
2024-08-12 08:28:56 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Digital Store
+
+<div align="center">
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-C92071)](https://opensource.org/licenses/MIT)
+[![Typescript Version](https://img.shields.io/badge/Typescript-5%2B-C92071)](https://www.typescriptlang.org/)
+[![GitHub repo size](https://img.shields.io/github/repo-size/marllonmendez/stories?color=C92071)]()
+[![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/marllonmendez/stories?color=C92071)]()
+
+[![React.js](https://img.shields.io/badge/React-C92071?style=for-the-badge&logo=react&logoColor=white)](https://react.dev/)
+[![TypeScript](https://img.shields.io/badge/TypeScript-C92071?style=for-the-badge&logo=typescript&logoColor=white)](https://www.typescriptlang.org/)
+[![Tailwind CSS](https://img.shields.io/badge/Tailwind_CSS-C92071?style=for-the-badge&logo=tailwind-css&logoColor=white)](https://tailwindcss.com/)
+[![React Icons](https://img.shields.io/badge/React%20Icons-C92071?style=for-the-badge&logo=react&logoColor=white)](https://react-icons.github.io/react-icons/)
+[![Prettier](https://img.shields.io/badge/prettier-C92071?style=for-the-badge&logo=prettier&logoColor=white)](https://prettier.io/)
+[![ESLint](https://img.shields.io/badge/eslint-C92071?style=for-the-badge&logo=eslint&logoColor=white)](https://eslint.org/)
+[![Vite](https://img.shields.io/badge/Vite-C92071?style=for-the-badge&logo=vite&logoColor=white)](https://vitejs.dev/)
+
+</div>
+
+## Sobre
+Este desafio faz parte da formação **Desenvolvedor Web Full Stack** que é um projeto do Instituto Euvaldo Lodi (IEL) e o Governo do Estado do Ceará por meio da Agência de Desenvolvimento do Estado do Ceará (ADECE) para capacitação de jovens na área da tecnologia.
+
+### Branches
+
+- **main:** Configurada para produção, integrando com os dados reais do back-end.
+- **develop:** Configurada com dados simulados (mock) para uma prévia do funcionamento da aplicação.
+
+
+[![Hero Section](src/assets/hero-section.png)]()
+
+## Guia de Instalação
+
+<h4>1. Clonagem do Repositório</h4>
+
+```bash
+git clone https://github.com/marllonmendez/digital-store.git
+```
+
+<h4>2. Instalação de Dependências</h4>
+
+```bash
+npm install
+```
+
+<h4>4. Execução Local</h4>
+
+```bash
+npm run dev
+```
+
+## Informações
+Para mais detalhes sobre o projeto desenvolvido, consulte os seguintes recursos:
+
+- [Documentação oficial](https://github.com/digitalcollegebr/projeto-digital-store)
+- [E-commerce Digital Store](https://digital-store-web.vercel.app/)
+
+
+## Licença
+
+Este projeto está licenciado sob a [Licença MIT](LICENSE).
---
+++ src/mock/index.tsx
@@ -0,0 +1,291 @@
+import { IProductCard } from '@/interface'
+import {
+  ProductCategory,
+  ProductGender,
+  ProductSession,
+  ProductBrand,
+} from '@/enum'
+
+export const Products: IProductCard[] = [
+  {
+    id: 1,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: 'Nike Air Jordan 11 Retro "Space Jam"',
+    price: 2199.99,
+    discount: 20,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/008/654/900/original/52015_00.png.png',
+    description:
+      'Clássico tênis Nike Air Jordan, perfeito para um look casual e confortável.',
+    slug: 'nike-air-jordan-11-retro-space-jam',
+  },
+  {
+    id: 2,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: "Nike Air Jordan 11 Retro 'Win Like '96'",
+    price: 1499.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/008/870/353/original/235806_00.png.png',
+    description:
+      'Tênis Nike Air Jordan 11 com design moderno, ideal para uso diário e casual.',
+    slug: 'nike-air-jordan-11-retro-win-like-96',
+  },
+  {
+    id: 3,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: "Nike Air Jordan 11 Retro 'Legend Blue'",
+    price: 1499.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/010/223/048/original/13607_00.png.png',
+    description:
+      'Design elegante e conforto garantido com o Nike Air Jordan 11 Legend Blue.',
+    slug: 'nike-air-jordan-11-retro-legend-blue',
+  },
+  {
+    id: 4,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.FEMALE,
+    name: "Nike Air Jordan 11 Retro Low 'Pink Snakeskin'",
+    price: 1499.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/021/357/907/original/478963_00.png.png',
+    description:
+      'Tênis estiloso com detalhe em pink snakeskin, ideal para um look diferenciado.',
+    slug: 'nike-air-jordan-11-retro-low-pink-snakeskin',
+  },
+  {
+    id: 5,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: "Nike Air Jordan 11 Retro Low 'Navy Snakeskin'",
+    price: 1499.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/020/310/667/original/459705_00.png.png',
+    description:
+      'Combine conforto e estilo com o Nike Air Jordan 11 Navy Snakeskin.',
+    slug: 'nike-air-jordan-11-retro-low-navy-snakeskin',
+  },
+  {
+    id: 6,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: "Nike Air Jordan 11 Retro Low 'Light Bone Snakeskin'",
+    price: 1499.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/021/219/116/original/CD6846_002.png.png',
+    description:
+      'Tênis leve e confortável com detalhe em snakeskin, perfeito para qualquer ocasião.',
+    slug: 'nike-air-jordan-11-retro-low-light-bone-snakeskin',
+  },
+  {
+    id: 7,
+    type: 'Tênis',
+    session: ProductSession.CHILD,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.UNISEX,
+    name: "Nike Sneaker Politics x Air Jordan 1 Low 'Block Party's'",
+    price: 5490.99,
+    discount: 50,
+    size: '30, 31, 32, 33, 34, 35',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/021/317/617/original/SP_AJ1_LOW_BP.png.png',
+    description:
+      'Design único e confortável, ideal para quem busca estilo e exclusividade.',
+    slug: 'nike-sneaker-politics-x-air-jordan-1-low-block-partys',
+  },
+  {
+    id: 8,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.UNISEX,
+    name: "Nike OFF-WHITE x Air Jordan 1 Retro High OG 'UNC'",
+    price: 5490.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/012/219/518/original/335047_00.png.png',
+    description:
+      'Colaboração exclusiva Nike OFF-WHITE, perfeito para um look sofisticado.',
+    slug: 'nike-off-white-x-air-jordan-1-retro-high-og-unc',
+  },
+  {
+    id: 9,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.UNISEX,
+    name: "Nike Travis Scott x Air Jordan 4 Retro 'Cactus Jack'",
+    price: 9999.99,
+    discount: 30,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/012/478/518/original/365514_00.png.png',
+    description:
+      'Tênis icônico em colaboração com Travis Scott, ideal para colecionadores.',
+    slug: 'nike-travis-scott-x-air-jordan-4-retro-cactus-jack',
+  },
+  {
+    id: 10,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: "Nike Air Jordan 1 Retro High OG Spider Man 'Origin Story'",
+    price: 1999.99,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/016/867/969/original/478648_00.png.png',
+    description:
+      'Inspire-se no Homem-Aranha com o Nike Air Jordan 1 Origin Story, super estiloso.',
+    slug: 'nike-air-jordan-1-retro-high-og-spider-man-origin-story',
+  },
+  {
+    id: 11,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.FEMALE,
+    name: "Nike Air Jordan 1 Retro High OG 'Turbo Green'",
+    price: 2149.9,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/018/552/901/original/404758_00.png.png',
+    description:
+      'Tênis com design moderno e cores vibrantes, perfeito para um look casual.',
+    slug: 'nike-air-jordan-1-retro-high-og-turbo-green',
+  },
+  {
+    id: 12,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.FEMALE,
+    name: "Nike Air Jordan 1 Retro High OG 'Crimson Tint'",
+    price: 1499.99,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/020/249/059/original/485842_00.png.png',
+    description:
+      'Tênis feminino com cores vibrantes, ideal para quem busca estilo e conforto.',
+    slug: 'nike-air-jordan-1-retro-high-og-crimson-tint',
+  },
+  {
+    id: 13,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.CASUAL,
+    brand: ProductBrand.NIKE,
+    gender: ProductGender.MALE,
+    name: "Nike Air Jordan 1 Retro High SB 'Lakers'",
+    price: 3640.9,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/020/270/533/original/CD6578_507.png.png',
+    description:
+      'Tênis inspirado nos Lakers, perfeito para fãs de basquete e colecionadores.',
+    slug: 'nike-air-jordan-1-retro-high-sb-lakers',
+  },
+  {
+    id: 14,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.SPORT,
+    brand: ProductBrand.ADIDAS,
+    gender: ProductGender.UNISEX,
+    name: "Adidas Yeezy Boost 350 V2 'Black Non-Reflective'",
+    price: 1399.99,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/020/624/696/original/FU9013.png.png',
+    description:
+      'Tênis Adidas Yeezy Boost, conforto e estilo em um design exclusivo e moderno.',
+    slug: 'adidas-yeezy-boost-350-v2-black-non-reflective',
+  },
+  {
+    id: 15,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.SPORT,
+    brand: ProductBrand.ADIDAS,
+    gender: ProductGender.UNISEX,
+    name: "Adidas Yeezy Boost 350 V2 'Beluga 2.0'",
+    price: 5999.99,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/008/654/534/original/152982_00.png.png',
+    description:
+      'Tênis Adidas Yeezy Beluga 2.0, estilo e conforto para qualquer ocasião.',
+    slug: 'adidas-yeezy-boost-350-v2-beluga-2-0',
+  },
+  {
+    id: 16,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.SPORT,
+    brand: ProductBrand.ADIDAS,
+    gender: ProductGender.UNISEX,
+    name: "Adidas Yeezy Boost 350 V2 'Sesame'",
+    price: 6348.99,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/014/037/000/original/357808_00.png.png',
+    description:
+      'Design moderno e conforto absoluto com o Adidas Yeezy Boost 350 V2 Sesame.',
+    slug: 'adidas-yeezy-boost-350-v2-sesame',
+  },
+  {
+    id: 17,
+    type: 'Tênis',
+    session: ProductSession.ADULT,
+    category: ProductCategory.SPORT,
+    brand: ProductBrand.ADIDAS,
+    gender: ProductGender.UNISEX,
+    name: "Adidas Yeezy Boost 350 V2 'Blue Tint'",
+    price: 6933.99,
+    size: '38, 39, 40, 41, 42, 43',
+    image:
+      'https://image.goat.com/attachments/product_template_pictures/images/008/707/328/original/152603_00.png.png',
+    description:
+      'Conforto e design inovador com o Adidas Yeezy Boost 350 V2 Blue Tint.',
+    slug: 'adidas-yeezy-boost-350-v2-blue-tint',
+  },
+]
